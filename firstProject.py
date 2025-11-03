b175fa446b138f66ca52fcf54484859b7d83c4d4
fahadtariq141613-ux <fahadtariq141613@gmail.com>
2025-11-03 12:00:22 +0500

    thirdproject.py

--- firstProject.py
+++ firstProject.py
@@ -1,1 +1,17 @@
-print("Hello,world!")    
+# Get two numbers from the user
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+third = int(input("Enter the second number: "))
+forth = int(input("Enter the second number: "))
+fifth = int(input("Enter the second number: "))
+sixth = int(input("Enter the second number: "))
+seventh = int(input("Enter the second number: "))
+eighth = int(input("Enter the second number: "))
+nineth = int(input("Enter the second number: "))
+tenth = int(input("Enter the second number: "))
+
+# Add the two numbers
+sum = first + second + third + forth + fifth + sixth + seventh + eighth + nineth + tenth
+
+# Show the result
+print("The sum is:", sum)
